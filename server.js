81d902e7ce370a8cdd50173e420bdb4954cb4640
Aco Mitevski <aco.mitevski@gmail.com>
2016-11-29 22:15:06 +0100

    fixes heroku deploy
    start prod server and use PORT env var

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const compression = require('compression')
 const api = require('./api/routes');
+const port = process.env.PORT || 3000;
 
 app.use(compression());
 app.use('/api', api);
 app.use('/', express.static('dist'));
 app.use('*', express.static('dist'));
 
-app.listen(3000, function () {
-  console.log('sf app listening on port 3000!');
+app.listen(port, function () {
+  console.log(`sf app listening on port ${port}!`);
 });
